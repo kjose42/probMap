42d1b628ba0d5b52f327f483b0ed3da41f5f63d9
Kelli Jose <88864336+kjose42@users.noreply.github.com>
2022-11-23 00:01:54 -0500

    Update probMap.py

--- probMap.py
+++ probMap.py
@@ -124,86 +124,88 @@ def createGrid(c, r, content, inNum):
 			temp_block = block([i+1, j+1], gridB[i][j], gridH[i][j], gridT[i][j])
 			blk_lst.append(temp_block)
 
-	f = open(f'groundTruth{inNum}.txt', "x")
-	startx = random.randint(1, c)
-	starty = random.randint(1, r)
-	while blk_lst[startx*starty - 1].blocked == True: #change coords if starting block is blocked
+	for i in range(10):
+		f = open(f'InfoFile{i}ForMap{inNum}.txt', "x")
 		startx = random.randint(1, c)
 		starty = random.randint(1, r)
-	f.write("(x0, y0) = (" + str(startx) + "," + str(starty) + ")\n")
-	f.write("(xi, yi):\n")
-	curx = startx
-	cury = starty
-	moveArr = []
-	sensorArr = []
-	for inc in range(100): #generating ground truth states
-		move = random.randint(1, 4) #1 = U, 2 = L, 3 = D, 4 = R
-		moveChar = ' '
-		oldx = curx
-		oldy = cury
-		#updating coords
-		if move == 1:
-			moveChar = 'U'
-			if cury != 1:
-				cury = cury - 1
-		if move == 2:
-			moveChar = 'L'
-			if curx != 1:
-				curx = curx - 1
-		if move == 3:
-			moveChar = 'D'
-			if cury != 50:
-				cury = cury + 1
-		if move == 4:
-			moveChar = 'R'
-			if curx != 100:
-				curx = curx + 1
-		moveFail = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1] # 10% chance of move failing
-		movef = random.choice(moveFail)
-		#if movef == 1:
-			#print("fail")
-		if gridB[curx - 1][cury - 1] == 1 or movef == 1: #checking if its trying to move to a blocked block
-			#print("blocked")
-			curx = oldx
-			cury = oldy
-		moveArr.append(moveChar)
-		#print("*" + str(curx) + " " + str(cury) + "*")
-		f.write("(" + str(curx) + "," + str(cury) + ")\n")
+		while blk_lst[startx*starty - 1].blocked == True: #change coords if starting block is blocked
+			startx = random.randint(1, c)
+			starty = random.randint(1, r)
+		f.write("(x0, y0) = (" + str(startx) + "," + str(starty) + ")\n")
+		f.write("(xi, yi):\n")
+		curx = startx
+		cury = starty
+		moveArr = []
+		sensorArr = []
+		for inc in range(100): #generating ground truth states
+			move = random.randint(1, 4) #1 = U, 2 = L, 3 = D, 4 = R
+			moveChar = ' '
+			oldx = curx
+			oldy = cury
+			#updating coords
+			if move == 1:
+				moveChar = 'U'
+				if cury != 1:
+					cury = cury - 1
+			if move == 2:
+				moveChar = 'L'
+				if curx != 1:
+					curx = curx - 1
+			if move == 3:
+				moveChar = 'D'
+				if cury != 50:
+					cury = cury + 1
+			if move == 4:
+				moveChar = 'R'
+				if curx != 100:
+					curx = curx + 1
+			moveFail = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1] # 10% chance of move failing
+			movef = random.choice(moveFail)
+			#if movef == 1:
+				#print("fail")
+			if gridB[curx - 1][cury - 1] == 1 or movef == 1: #checking if its trying to move to a blocked block
+				#print("blocked")
+				curx = oldx
+				cury = oldy
+			moveArr.append(moveChar)
+			#print("*" + str(curx) + " " + str(cury) + "*")
+			f.write("(" + str(curx) + "," + str(cury) + ")\n")
 		
-		actualType = 'N'
-		otherTypes = ['H', 'T']
-		if gridH[curx - 1][cury - 1] == 1:
-			actualType = 'H'
-			otherTypes = ['N', 'T']
-		if gridT[curx - 1][cury - 1] == 1:
-			actualType = 'T'
-			otherTypes = ['N', 'H']
-		print(actualType)
-		senseFail1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] #5% chance of failing and sensing other type
-		senseFail2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] 
-		senseF1 = random.choice(senseFail1)
-		senseF2 = random.choice(senseFail2)
-		if senseF1 == 1:
-			#print("fail1")
-			sensorArr.append(otherTypes[0])
-		elif senseF2 == 1:
-			#print("fail2")
-			sensorArr.append(otherTypes[1])
-		else:
-			sensorArr.append(actualType)
-				
+			actualType = 'N'
+			otherTypes = ['H', 'T']
+			if gridH[curx - 1][cury - 1] == 1:
+				actualType = 'H'
+				otherTypes = ['N', 'T']
+			if gridT[curx - 1][cury - 1] == 1:
+				actualType = 'T'
+				otherTypes = ['N', 'H']
+			print(actualType)
+			senseFail1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] #5% chance of failing and sensing other type
+			senseFail2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] 
+			senseF1 = random.choice(senseFail1)
+			senseF2 = random.choice(senseFail2)
+			if senseF1 == 1:
+				print("fail1")
+				sensorArr.append(otherTypes[0])
+			elif senseF2 == 1:
+				print("fail2")
+				sensorArr.append(otherTypes[1])
+			else:
+				sensorArr.append(actualType)
+					
 		
 
-	#generating actions
-	f.write("a:\n")
-	for inc in range(100):
-		f.write(moveArr[inc] + "\n")
+		#generating actions
+		f.write("a:\n")
+		for inc in range(100):
+			f.write(moveArr[inc] + "\n")
 
-	#generating sensor readings
-	f.write("e:\n")
-	for inc in range(100):
-		f.write(sensorArr[inc] + "\n")
-	f.close()
+		#generating sensor readings
+		f.write("e:\n")
+		for inc in range(100):
+			f.write(sensorArr[inc] + "\n")
+
+		f.close()
 	
 	root.resizable(True, True)
 	root.mainloop()
@@ -214,6 +216,6 @@ def main():
 	testFile = open(f'testcase{textnum}.txt', "r")
 	content = testFile.readlines()
 	col, row = content[2].split()
-	createGrid(int(col), int(row), content, inNum)
+	createGrid(int(col), int(row), content, textNum)
 
 main();
