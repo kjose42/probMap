3fd0eec9dba50d1180ef38ceb744acae9503a276
Kelli Jose <88864336+kjose42@users.noreply.github.com>
2022-12-14 00:57:24 -0500

    Update probMap.py

--- probMap.py
+++ probMap.py
@@ -5,19 +5,23 @@ from array import *
 import random
 
 class block:
-	def __init__(self, position, blk, hi, trav):
-		if blk == 1:
+	def __init__(self, position, blockType):
+		if blockType == 'B':
 			self.blocked = True
 		else:
 			self.blocked = False
-		if hi == 1:
+		if blockType == 'H':
 			self.highway = True
 		else:
 			self.highway = False
-		if trav == 1:
+		if blockType == 'T':
 			self.traverse = True
 		else:
 			self.traverse = False
+		if blockType == 'N':
+			self.normal = True
+		else:
+			self.normal = False
 		self.x = position[0]
 		self.y = position[1]
 
@@ -58,13 +62,11 @@ def createGrid(c, r, content, inNum):
 
 	#-------------------------------------------------------------------------------------------
 	#Drawing the Grid (sqaures, vertices, start)
-	gridB = []
-	gridH = []
-	gridT = []
-
+	gridT = [] #grid indicating square type
 	#i represents the current grid square, used to check square's boolean var
 	i = 2
 	for x in range(0, c):
+		tmp_arrT = []
 		for y in range(0, r):
 			cx, cy, b, h, t = content[i].split()
 			x1 = ((int(cx)-1) * 50 + 25)
@@ -74,40 +76,20 @@ def createGrid(c, r, content, inNum):
 			b = b.strip()
 			my_canvas.create_rectangle(x1,y1,y2,x2)
 			if b == '1': #square is blocked
+				tmp_arrT.append('B')
 				my_canvas.create_rectangle(x1,y1,y2,x2,fill='gray')
 			if h == '1': #square is highway
+				tmp_arrT.append('H')
 				my_canvas.create_rectangle(x1,y1,y2,x2,fill='yellow')
 			if t == '1': #square is "hard to traverse"
+				tmp_arrT.append('T')
 				my_canvas.create_rectangle(x1,y1,y2,x2,fill='purple')
+			if b == '0' and h == '0' and t == '0':
+				tmp_arrT.append('N')
 			i = i + 1
 			my_canvas.create_rectangle(x1,y1,y2,x2)
-		
-
-	i = 2
-	for co in range(0, c):
-		tmp_arrB = []
-		tmp_arrH = []
-		tmp_arrT = []
-		for ro in range(0, r):
-			cx, cy, b, h, t = content[i].split()
-			if b == '0': #if bool == 0, then square is unblocked
-				tmp_arrB.append(0)
-			else: #square is blocked
-				tmp_arrB.append(1)
-			if h == '0':
-				tmp_arrH.append(0)
-			else: #square is highway
-				tmp_arrH.append(1)
-			if t == '0':
-				tmp_arrT.append(0)
-			else: #square is "hard to traverse"
-				tmp_arrT.append(1)
-			i = i + 1
-		gridB.append(tmp_arrB)
-		gridH.append(tmp_arrH)
 		gridT.append(tmp_arrT)
 
-	
 	#labelling x axis
 	for x in range(0, c):
 		text = my_canvas.create_text(x * 50 + 50, 10, text=x+1, tags="text")
@@ -121,10 +103,10 @@ def createGrid(c, r, content, inNum):
 	blk_lst = []
 	for i in range(c):
 		for j in range(r):
-			temp_block = block([i+1, j+1], gridB[i][j], gridH[i][j], gridT[i][j])
+			temp_block = block([i+1, j+1], gridT[i][j])
 			blk_lst.append(temp_block)
 
-	for i in range(10):
+	for i in range(100):
 		f = open(f'InfoFile{i}ForMap{inNum}.txt', "x")
 		startx = random.randint(1, c)
 		starty = random.randint(1, r)
@@ -153,15 +135,15 @@ def createGrid(c, r, content, inNum):
 					curx = curx - 1
 			if move == 3:
 				moveChar = 'D'
-				if cury != 50:
+				if cury != r:
 					cury = cury + 1
 			if move == 4:
 				moveChar = 'R'
-				if curx != 100:
+				if curx != c:
 					curx = curx + 1
 			moveFail = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1] # 10% chance of move failing
 			movef = random.choice(moveFail)
-			if gridB[curx - 1][cury - 1] == 1 or movef == 1: #checking if its trying to move to a blocked block
+			if gridT[curx - 1][cury - 1] == 'B' or movef == 1: #checking if its trying to move to a blocked block
 				curx = oldx
 				cury = oldy
 			moveArr.append(moveChar)
@@ -169,10 +151,10 @@ def createGrid(c, r, content, inNum):
 		
 			actualType = 'N'
 			otherTypes = ['H', 'T']
-			if gridH[curx - 1][cury - 1] == 1:
+			if gridT[curx - 1][cury - 1] == 'H':
 				actualType = 'H'
 				otherTypes = ['N', 'T']
-			if gridT[curx - 1][cury - 1] == 1:
+			if gridT[curx - 1][cury - 1] == 'T':
 				actualType = 'T'
 				otherTypes = ['N', 'H']
 			senseFail1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] #5% chance of failing and sensing other type
@@ -199,10 +181,9 @@ def createGrid(c, r, content, inNum):
 			f.write(sensorArr[inc] + "\n")
 
 		f.close()
-	
+
 	root.resizable(True, True)
 	root.mainloop()
-
 
 def main():
 	textnum = input('Enter the number of the testcase that you want to run: ')
@@ -210,5 +191,4 @@ def main():
 	content = testFile.readlines()
 	col, row = content[1].split()
 	createGrid(int(col), int(row), content, textnum)
-
 main();
